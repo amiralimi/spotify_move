dffe0c22f812e313bde85881afccec507b227b9c
amirali mirian <amirali.mirian@gmail.com>
2021-12-19 00:22:58 +0330

    added argparse to move.py.

--- move.py
+++ move.py
@@ -1,6 +1,13 @@
+import argparse
 from do_export import export
 from do_import import import_
 
 if __name__ == '__main__':
-    # export()
-    import_()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--import_', help='import songs into exported_data.json', action='store_true')
+    parser.add_argument('-e', '--export', help='export songs from exported_data.json', action='store_true')
+    args = parser.parse_args()
+    if args.export:
+        export()
+    if args.import_:
+        import_()
